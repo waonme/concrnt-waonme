b7cd52f049e4312c2995186a699f892d71570395
rassi0429 <taitokokoa+rassi@gmail.com>
2025-01-23 18:15:51 +0900

    feat: [wip] カードにしてみた

--- src/pages/ExplorerPlus.tsx
+++ src/pages/ExplorerPlus.tsx
@@ -2,6 +2,7 @@ import { Helmet } from 'react-helmet-async'
 import {
     Avatar,
     Box,
+    Button,
     Collapse,
     Divider,
     IconButton,
@@ -18,8 +19,7 @@ import {
 } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useCallback, useEffect, useRef, useState } from 'react'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import { StreamCard } from '../components/Stream/Card'
 
 export interface Domain {
     fqdn: string
@@ -54,8 +54,6 @@ export interface DomainMeta {
     vapidKey: string
 }
 
-// document
-
 export interface Timeline {
     id: string
     indexable: boolean
@@ -64,7 +62,7 @@ export interface Timeline {
     schema: string
     policy: string
     policyParams: string
-    document: string
+    document?: string
     _parsedDocument: {
         id: string
         owner: string
@@ -75,6 +73,7 @@ export interface Timeline {
             name: string
             shortname: string
             description: string
+            banner: string
         }
         meta: {
             client: string
@@ -87,28 +86,43 @@ export interface Timeline {
     signature: string
     cdate: string
     mdate: string
-}
-
-export interface DomainCache {
-    domain: Domain
-    timelines: Timeline[]
+    domainFQDN?: string
 }
 
 export function ExplorerPlusPage(): JSX.Element {
     const { t } = useTranslation('', { keyPrefix: 'pages.explore' })
     const theme = useTheme()
 
-    const [domains, setDomains] = useState<DomainCache[]>([])
+    const [timelines, setTimelines] = useState<Timeline[]>([])
+    const [domains, setDomains] = useState<Domain[]>([])
+
     const [query, setQuery] = useState('')
 
     const [textArea, setTextArea] = useState('')
 
     useEffect(() => {
         // fetch
-        fetch('https://c.kokopi.me/cache?q=' + query).then(async (result) => {
+        if (query === '') {
+            fetch('https://c.kokopi.me/timeline?random=true&limit=30').then(async (result) => {
+                setTimelines(await result.json())
+            })
+        } else {
+            fetch('https://c.kokopi.me/timeline?q=' + query).then(async (result) => {
+                setTimelines(await result.json())
+            })
+        }
+
+        fetch('https://c.kokopi.me/domain').then(async (result) => {
             setDomains(await result.json())
         })
     }, [query])
+
+    const getDomainFromFQDN = useCallback(
+        (fqdn: string) => {
+            return domains.filter((d) => d.fqdn === fqdn)[0]
+        },
+        [domains]
+    )
 
     return (
         <>
@@ -130,85 +144,81 @@ export function ExplorerPlusPage(): JSX.Element {
                 <Box>
                     <Typography variant="h2">{t('title')}</Typography>
                     <Divider sx={{ mb: 1 }} />
-                    <h3>{domains.length}</h3>
-
                     <TextField
                         value={textArea}
                         onChange={(e) => {
                             setTextArea(e.target.value)
                             setQuery(e.target.value)
                         }}
-                    ></TextField>
+                    />
+
+                    <Box
+                        sx={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+                            gap: 2
+                        }}
+                    >
+                        {timelines.map((t) => {
+                            return (
+                                <StreamCard
+                                    key={t.id}
+                                    streamID={t.id}
+                                    name={t._parsedDocument.body.name}
+                                    description={t._parsedDocument.body.description}
+                                    banner={t._parsedDocument.body.banner}
+                                    domain={t.domainFQDN ?? ''}
+                                />
+                            )
+                        })}
+                    </Box>
                 </Box>
-                <TableContainer component={Paper}>
-                    <Table aria-label="collapsible table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell width={10} />
-                                <TableCell>NickName</TableCell>
-                                <TableCell>FQDN</TableCell>
-                                <TableCell align="right">Count Of Stream</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {domains.map((row) => (
-                                <Row key={row.domain.fqdn} domain={row} />
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
             </Box>
         </>
     )
 }
 
-function Row(props: { domain: DomainCache }): JSX.Element {
-    const [open, setOpen] = useState(true)
-    const { domain } = props
-    console.log(props)
+function Row(props: { timeline: Timeline; domain: Domain }): JSX.Element {
+    const { timeline, domain } = props
     return (
         <>
             <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => {
-                            setOpen(!open)
-                        }}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
+                <TableCell component="th" scope="row" colSpan={1}>
+                    <Typography>#{timeline._parsedDocument.body.name}</Typography>
                 </TableCell>
-                <TableCell component="th" scope="row">
+                <TableCell colSpan={3}>
+                    <Typography variant={'caption'}>{timeline._parsedDocument.body.description}</Typography>
+                </TableCell>
+                <TableCell colSpan={1}>
                     <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-                        <Avatar alt={domain.domain.meta?.nickname} src={domain.domain.meta?.logo} />
-                        <Typography>{domain.domain.meta?.nickname}</Typography>
+                        <Avatar src={domain?.meta?.logo} sx={{ height: 24, width: 24 }} />
+                        <Typography variant="caption">{domain?.meta?.nickname}</Typography>
                     </Box>
                 </TableCell>
-                <TableCell>{domain.domain.fqdn}</TableCell>
-                <TableCell align="right">{domain.timelines.length}</TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0, paddingLeft: '80px' }} colSpan={6}>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box sx={{ margin: 1 }}>
-                            <Table size="small" aria-label="purchases">
-                                <TableBody>
-                                    {domain.timelines.map((timeline) => (
-                                        <TableRow key={timeline.id}>
-                                            <TableCell component="th" scope="row">
-                                                {timeline._parsedDocument.body.name}
-                                            </TableCell>
-                                            <TableCell>{timeline._parsedDocument.body.description}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </Box>
-                    </Collapse>
+                <TableCell colSpan={1}>
+                    <Button size={'small'}>見てみる</Button>
                 </TableCell>
             </TableRow>
+            {/* <TableRow> */}
+            {/*    <TableCell style={{ paddingBottom: 0, paddingTop: 0, paddingLeft: '80px' }} colSpan={6}> */}
+            {/*        <Collapse in={open} timeout="auto" unmountOnExit> */}
+            {/*            <Box sx={{ margin: 1 }}> */}
+            {/*                <Table size="small" aria-label="purchases"> */}
+            {/*                    <TableBody> */}
+            {/*                        {domain.timelines.map((timeline) => ( */}
+            {/*                            <TableRow key={timeline.id}> */}
+            {/*                                <TableCell component="th" scope="row"> */}
+            {/*                                    {timeline._parsedDocument.body.name} */}
+            {/*                                </TableCell> */}
+            {/*                                <TableCell>{timeline._parsedDocument.body.description}</TableCell> */}
+            {/*                            </TableRow> */}
+            {/*                        ))} */}
+            {/*                    </TableBody> */}
+            {/*                </Table> */}
+            {/*            </Box> */}
+            {/*        </Collapse> */}
+            {/*    </TableCell> */}
+            {/* </TableRow> */}
         </>
     )
 }
